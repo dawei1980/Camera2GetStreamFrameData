2b15a73ff73624c3343e3f03fe0b4f1f7884e31e
蒋大卫 <10159757jiang_dawei@cn.tre-inc.com>
2019-05-24 08:58:47 +0800

    Add AI model

--- app/src/main/java/com/stream/frame/utils/CameraSizeUtil.java
+++ app/src/main/java/com/stream/frame/utils/CameraSizeUtil.java
@@ -5,7 +5,6 @@ import android.util.Size;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class CameraSizeUtil {
@@ -61,14 +60,13 @@ public class CameraSizeUtil {
     /**
      * Compares two {@code Size}s based on their areas.
      */
-    public static class CompareSizesByArea implements Comparator<Size> {
-
-        @Override
-        public int compare(Size lhs, Size rhs) {
-            // We cast here to ensure the multiplications won't overflow
-            return Long.signum((long) lhs.getWidth() * lhs.getHeight() -
-                    (long) rhs.getWidth() * rhs.getHeight());
-        }
-
-    }
+//    public static class CompareSizesByArea implements Comparator<Size> {
+//
+//        @Override
+//        public int compare(Size lhs, Size rhs) {
+//            // We cast here to ensure the multiplications won't overflow
+//            return Long.signum((long) lhs.getWidth() * lhs.getHeight() -
+//                    (long) rhs.getWidth() * rhs.getHeight());
+//        }
+//    }
 }
